363c8eebde01bc0660424e261003cadf36dc3593
emre-5454 <emre_sakarya_513@hotmail.com>
2026-04-25 18:02:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 TELEGRAM_TOKEN = "8637824602:AAG8V2VJ3QM0WI40PUpu1zbT-67qCpWgbOQ"
 CHAT_ID = "6977265844"
 
+  
 @app.route('/', methods=['POST'])
 def webhook():
-    data =request.get_json(silent=True) or request.data.decode("utf-8") or "TradingView alarmı geldi"
-    
+    data =request.get_json(silent=True) or request.data.decode("utf-8") or "TradingView alarmı geldi"  
     message = f"ALARM GELDİ 🚨\n\n{data}"
     
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
