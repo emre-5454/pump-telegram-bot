92271b050ce290cb0f2bb9bbc1c0a338b5152c93
emre-5454 <emre_sakarya_513@hotmail.com>
2026-04-25 18:08:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 TELEGRAM_TOKEN = "8637824602:AAG8V2VJ3QM0WI40PUpu1zbT-67qCpWgbOQ"
 CHAT_ID = "6977265844"
 
-  
-@app.route('/', methods=['POST'])
-def webhook():
+
 @app.route('/', methods=['GET', 'POST'])
 def webhook():
     if request.method == 'GET':
