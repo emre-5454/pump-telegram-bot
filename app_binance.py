aca4b6b38abd7209a4d75fc3a744709a98594611
emre-5454 <emre_sakarya_513@hotmail.com>
2026-04-27 23:45:15 +0300

    Update app_binance.py

--- app_binance.py
+++ app_binance.py
@@ -9,14 +9,13 @@ app = Flask(__name__)
 
 TELEGRAM_TOKEN = "8637824602:AAG8V2VJ3QM0WI40PUpu1zbT-67qCpWgbOQ"
 CHAT_ID = "6977265844"
-
 exchange = ccxt.binance({
     "enableRateLimit": True,
     "options": {
-        "defaultType": "spot"
+        "defaultType": "spot",
+        "defaultSubType": "spot"
     }
 })
-
 TIMEFRAME = "5m"
 LIMIT = 6
 
