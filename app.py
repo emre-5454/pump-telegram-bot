bfe34413dc7a60f9536f6f7eef9672a846e0e605
emre-5454 <emre_sakarya_513@hotmail.com>
2026-05-02 22:37:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,17 @@ import os
 
 app = Flask(__name__)
 
-TELEGRAM_TOKEN = "8637824602:AAG8V2VJ3QM0WI40PUpu1zbT-67qCpWgbOQ"
+TELEGRAM_TOKEN = "8637824602:AAG8V2VJ3QM0WI40PUpu1zbT-67qCpWgbOQ
 CHAT_ID = "6977265844"
 
 exchange = ccxt.mexc({"enableRateLimit": True})
 
-MIN_LAST_VOLUME_USDT = 80_000
-MIN_VOLUME_MULTIPLIER = 4
-MIN_PRICE_CHANGE_15M = 0.3
-MAX_PRICE_CHANGE_15M = 2.0
+MIN_LAST_VOLUME_USDT = 60_000
+MIN_VOLUME_MULTIPLIER = 3
+MIN_PRICE_CHANGE_15M = 0.2
+MAX_PRICE_CHANGE_15M = 2.5
+MIN_BODY_RATIO = 0.4
+
 COOLDOWN_SECONDS = 60 * 60
 HEARTBEAT_SECONDS = 30 * 60
 
@@ -39,6 +41,22 @@ def scan_symbol(symbol):
 
     change_15m = ((close_now - open_15m) / open_15m) * 100
 
+    open_price = last[1]
+    high = last[2]
+    low = last[3]
+    close_price = last[4]
+
+    candle_range = high - low
+    body = abs(close_price - open_price)
+
+    if candle_range == 0:
+        return None
+
+    body_ratio = body / candle_range
+
+    if body_ratio < MIN_BODY_RATIO:
+        return None
+
     last_volume_usdt = last[5] * close_now
     avg_prev_volume_usdt = sum(c[5] * c[4] for c in prev) / len(prev)
 
@@ -52,13 +70,15 @@ def scan_symbol(symbol):
         and volume_multiplier > MIN_VOLUME_MULTIPLIER
         and change_15m > MIN_PRICE_CHANGE_15M
         and change_15m < MAX_PRICE_CHANGE_15M
+        and close_price > open_price
     ):
         return {
             "symbol": symbol,
             "price": close_now,
             "change": change_15m,
             "volume": last_volume_usdt,
-            "multiplier": volume_multiplier
+            "multiplier": volume_multiplier,
+            "body_ratio": body_ratio
         }
 
     return None
@@ -99,6 +119,7 @@ Fiyat: {result['price']}
 Son 15dk Değişim: %{round(result['change'], 2)}
 Son 5dk Hacim: {round(result['volume']):,} USDT
 Hacim Artışı: {round(result['multiplier'], 2)}x
+Mum Gövde Gücü: %{round(result['body_ratio'] * 100, 1)}
 
 ⚠️ Kontrol:
 - 5m mum güçlü mü?
