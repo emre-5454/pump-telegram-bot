bdf8dac6ba756ea21250c0b300da7302d9d65bc0
emre-5454 <emre_sakarya_513@hotmail.com>
2026-05-25 22:08:08 +0300

    Update binance_bot.py

--- binance_bot.py
+++ binance_bot.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 BOT_NAME = "🚀 BINANCE RAILWAY FUTURES BOT"
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_TOKEN = "8637824602:AAG8V2VJ3QM0WI40PUpu1zbT-67qCpWgbOQ"
+CHAT_ID = "6977265844"
 
 BASE_URL = "https://fapi.binance.com"
 
